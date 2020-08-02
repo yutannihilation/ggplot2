522ed7f94b7f52ed425fb574f0256a67fb9931ed
Hiroaki Yutani <yutani.ini@gmail.com>
2020-08-02 21:39:25 +0900

    Update aaa-.r

--- R/aaa-.r
+++ R/aaa-.r
@@ -2,7 +2,7 @@
 #' @include ggproto.r
 NULL
 
-#' Base ggproto classes for ggplot2
+#' Foo
 #'
 #' If you are creating a new geom, stat, position, or scale in another package,
 #' you'll need to extend from `ggplot2::Geom`, `ggplot2::Stat`,
